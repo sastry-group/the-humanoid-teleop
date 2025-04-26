33e945b38eda003797de0bf39d8bf79ac2070881
Sastry (SDH 7th FL PC) <sastry.group.berkeley@gmail.com>
2025-04-25 17:23:08 -0700

    Initial teleop setup (need to debug event names and set up image server)

---
+++ teleop/TeleVision.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+
+import asyncio
+import numpy as np
+import time
+from vuer import Vuer
+from vuer.events import ClientEvent
+from vuer.schemas import ImageBackground, Hands, DefaultScene
+# from preprocessing.processing import VuerPreprocessor
+from multiprocessing import Array, Value, Process, shared_memory
+
+class TeleVision:
+    def __init__(self):
+        # def __init__(self, img_shm_name, img_shape):
+        # Initialize the shared memory for the image
+        # self.img_height = img_shape[0]
+        # self.img_width  = img_shape[1] // 2
+        # existing_shm = shared_memory.SharedMemory(name=img_shm_name)
+        # self.img_array = np.ndarray(img_shape, dtype=np.uint8, buffer=existing_shm.buf)
+
+        # Instantiate the Vuer app WITH ngrok (no certs needed)
+        self.app = Vuer(host="0.0.0.0", queries=dict(grid=False), queue_len=3)
+
+        # Register our HAND_MOVE and CAM_MOVE handlers
+        self.app.add_handler("CAM_MOVE")(self.on_cam_move)
+        self.app.add_handler("HAND_MOVE")(self.on_hand_move)
+
+        # Spawn our very simple scene (hand skeleton overlay)
+        # self.app.spawn(start=False)(self.main_image)
+        self.app.spawn(start=False)(self.main_scene)
+
+        # # Start the Vuer app in a separate process to avoid blocking in the main thread
+        # self.process = Process(target=self.run)
+        # self.process.daemon = True
+        # self.process.start()
+
+        # Initialize the Vuer preprocessor
+        # self.vuer_preprocessor = VuerPreprocessor()
+
+        # Initalize shared memory arrays for hand and head transforms
+        self.left_hand_shared = Array('d', 16, lock=True)
+        self.right_hand_shared = Array('d', 16, lock=True)
+        self.left_landmarks_shared = Array('d', 75, lock=True)
+        self.right_landmarks_shared = Array('d', 75, lock=True)
+        self.head_matrix_shared = Array('d', 16, lock=True)
+        self.aspect_shared = Value('d', 1.0, lock=True)
+
+    def process(self, tv):
+        return self.vuer_preprocessor.process(tv)
+    
+    async def on_hand_move(self, event: ClientEvent, session, fps=60):
+        """
+        Handler for HAND_MOVE events. Updates the shared memory arrays with:
+        - leftWrist  (16 floats → 4x4 homogenous transformation matrix)
+        - rightWrist  (16 floats → 4x4 homogenous transformation matrix)
+        - leftLandmarks  (75 floats → 25x3 landmarks x/y/z array)
+        - rightLandmarks  (75 floats → 25x3 landmarks x/y/z array)
+        """
+        try:
+            # print("HAND_MOVE event value:", event.value)
+            # Update left hand transform
+            self.left_hand_shared[:] = np.array(event.value["left"]).flatten(order="F")
+            # Update right hand transform
+            self.right_hand_shared[:] = np.array(event.value["right"]).flatten(order="F")
+            # Update left hand landmarks
+            self.left_landmarks_shared[:] = np.array(event.value["leftLandmarks"]).flatten()
+            # Update right hand landmarks
+            self.right_landmarks_shared[:] = np.array(event.value["rightLandmarks"]).flatten()
+        except Exception as e:
+            print("Error in HAND_MOVE handler:", e)
+
+    async def on_cam_move(self, event: ClientEvent, session):
+        """
+        Handler for CAM_MOVE events. Updates the shared memory arrays with:
+        - camera (16 floats → 4x4 homogenous transformation matrix)
+        - aspect (1 float → aspect ratio of the camera)
+        """
+        try:
+            # Update head matrix
+            self.head_matrix_shared[:] = np.array(event.value["camera"]["matrix"]).flatten(order="F")
+            # Update aspect ratio
+            self.aspect_shared.value = event.value["camera"]["aspect"]
+        except Exception as e:
+            print("Error in CAM_MOVE handler:", e)
+
+    # async def main_image(self, session, fps=60):
+        # session.upsert @ Hands(fps=fps, stream=True, key="hands", showLeft=False, showRight=False)
+        # while True:
+        #     display_image = cv2.cvtColor(self.img_array, cv2.COLOR_BGR2RGB)
+        #     # aspect_ratio = self.img_width / self.img_height
+        #     session.upsert(
+        #         [
+        #             ImageBackground(
+        #                 display_image[:, :self.img_width],
+        #                 aspect=1.778,
+        #                 height=1,
+        #                 distanceToCamera=1,
+        #                 # The underlying rendering engine supported a layer binary bitmask for both objects and the camera. 
+        #                 # Below we set the two image planes, left and right, to layers=1 and layers=2. 
+        #                 # Note that these two masks are associated with left eye’s camera and the right eye’s camera.
+        #                 layers=1,
+        #                 format="jpeg",
+        #                 quality=50,
+        #                 key="background-left",
+        #                 interpolate=True,
+        #             ),
+        #             ImageBackground(
+        #                 display_image[:, self.img_width:],
+        #                 aspect=1.778,
+        #                 height=1,
+        #                 distanceToCamera=1,
+        #                 layers=2,
+        #                 format="jpeg",
+        #                 quality=50,
+        #                 key="background-right",
+        #                 interpolate=True,
+        #             ),
+        #         ],
+        #         to="bgChildren",
+        #     )
+        #     # 'jpeg' encoding should give you about 30fps with a 16ms wait in-between.
+        #     await asyncio.sleep(0.016 * 2)
+
+    async def main_scene(self, session, fps=60):
+        """
+        A minimal Vuer scene that just streams and draws your hands
+        so you can verify visually on the headset.
+        """
+        session.set @ DefaultScene(frameloop="always")
+        session.upsert @ Hands(fps=fps, stream=True)
+        while True:
+            await asyncio.sleep(1)
+
+
+    def run(self):
+        """
+        Starts the Vuer app and runs the event loop.
+        """
+        self.app.run()
+
+    def printPoses(self):
+        """
+        Prints the current hand and head poses.
+        """
+        print("Left Wrist:\n", self.left_hand)
+        print("Right Wrist:\n", self.right_hand)
+        print("Left Landmarks:\n", self.left_landmarks)
+        print("Right Landmarks:\n", self.right_landmarks)
+        print("Head Matrix:\n", self.head_mat)
+        print("Aspect Ratio:", self.aspect)
+        print("―" * 60)
+
+    @property
+    def left_hand(self): # Left hand 4x4 homogenous transformation matrix
+        return np.array(self.left_hand_shared[:]).reshape(4, 4, order="F")
+    
+    @property
+    def right_hand(self): # Right hand 4x4 homogenous transformation matrix
+        return np.array(self.right_hand_shared[:]).reshape(4, 4, order="F")
+    
+    @property
+    def head_mat(self): # Head 4x4 homogenous transformation matrix
+        return np.array(self.head_matrix_shared[:]).reshape(4, 4, order="F")
+    
+    @property
+    def left_landmarks(self): # Left hand 25x3 landmarks with x/y/z coordinates
+        return np.array(self.left_landmarks_shared[:]).reshape(25, 3)
+    
+    @property
+    def right_landmarks(self): # Right hand 25x3 landmarks with x/y/z coordinates
+        return np.array(self.right_landmarks_shared[:]).reshape(25, 3)
+    
+    @property
+    def aspect(self): # Aspect ratio of the camera
+        return float(self.aspect_shared.value)
+
+if __name__ == "__main__":
+    import os 
+    import sys
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    sys.path.append(parent_dir)
+    import threading
+    from image_server.image_client import ImageClient
+
+    # # image
+    # img_shape = (480, 640 * 2, 3)
+    # img_shm = shared_memory.SharedMemory(create=True, size=np.prod(img_shape) * np.uint8().itemsize)
+    # img_array = np.ndarray(img_shape, dtype=np.uint8, buffer=img_shm.buf)
+    # img_client = ImageClient(tv_img_shape = img_shape, tv_img_shm_name = img_shm.name)
+    # image_receive_thread = threading.Thread(target=img_client.receive_process, daemon=True)
+    # image_receive_thread.start()
+
+    # television
+    # tv = TeleVision(img_shape, img_shm.name)
+    tv = TeleVision()
+    tv.run()
+    print("vuer unit test program running...")
+    print("you can press ^C to interrupt program.")
+    while True:
+        time.sleep(1)
+        tv.printPoses()
---
+++ teleop/utils/MatrixTools.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def mat_update(prev_mat, mat):
+    if np.linalg.det(mat) == 0:
+        return prev_mat, False # Return previous matrix and False flag if the new matrix is singular (determinant = 0).
+    else:
+        return mat, True
+
+def fast_mat_inv(mat):
+    ret = np.eye(4)
+    ret[:3, :3] = mat[:3, :3].T
+    ret[:3, 3] = -mat[:3, :3].T @ mat[:3, 3]
+    return ret
